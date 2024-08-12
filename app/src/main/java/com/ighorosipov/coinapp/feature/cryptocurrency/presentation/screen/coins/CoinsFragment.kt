8d300b7ceeb624f837eac086ef135684ab4020c4
Igor Osipov <ighor.osipov.95@mail.ru>
2024-08-12 13:34:59 +0300

    Изменена фотографии криптовалюты. Изменен цвет фона для чипов. Добавлен фрагмент списка криптовалюты и XML разметка для него.

---
+++ app/src/main/java/com/ighorosipov/coinapp/feature/cryptocurrency/presentation/screen/coins/CoinsFragment.kt
@@ -0,0 +1,20 @@
+package com.ighorosipov.coinapp.feature.cryptocurrency.presentation.screen.coins
+
+import com.ighorosipov.coinapp.databinding.FragmentCoinsBinding
+import com.ighorosipov.coinapp.util.base.BaseFragment
+
+class CoinsFragment(override val viewModel: CoinsViewModel) : BaseFragment<FragmentCoinsBinding, CoinsViewModel> (
+    FragmentCoinsBinding::inflate
+) {
+    override fun inject() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initViews() {
+        TODO("Not yet implemented")
+    }
+
+    override fun subscribeToObservers() {
+        TODO("Not yet implemented")
+    }
+}
