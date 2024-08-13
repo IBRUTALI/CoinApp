ef062c632d793dbcf75623f4338ceab22c851a91
Igor Osipov <ighor.osipov.95@mail.ru>
2024-08-13 13:11:31 +0300

    Создан XML макет для экрана деталей. Добавлены ProgressBar, который будет отображатся, когда данные грузятся и экран ошибки.

---
+++ app/src/main/java/com/ighorosipov/coinapp/feature/cryptocurrency/presentation/screen/coin_detail/CoinDetailFragment.kt
@@ -0,0 +1,24 @@
+package com.ighorosipov.coinapp.feature.cryptocurrency.presentation.screen.coin_detail
+
+import com.ighorosipov.coinapp.databinding.FragmentCoinsBinding
+import com.ighorosipov.coinapp.util.base.BaseFragment
+
+class CoinDetailFragment: BaseFragment<FragmentCoinsBinding, CoinDetailViewModel>(
+    FragmentCoinsBinding::inflate
+) {
+    override val viewModel: CoinDetailViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun inject() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initViews() {
+        TODO("Not yet implemented")
+    }
+
+    override fun subscribeToObservers() {
+        TODO("Not yet implemented")
+    }
+
+}
